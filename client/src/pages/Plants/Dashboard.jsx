25515e0d25c8ff7cadd0d44d646bdb1cdc8a221c
Achyut <aoogleak@gmail.com>
2023-06-11 18:56:34 +0545

    map template added

--- client/src/pages/Plants/Dashboard.jsx
+++ client/src/pages/Plants/Dashboard.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "../../css/map.css";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+	const initialPosition = [27.69202, 84.43701];
+
+	return (
+		<>
+			<div>
+				<div className="mapContainer">
+					<h1>My Map</h1>
+					<MapContainer
+						center={initialPosition}
+						zoom={10}
+						scrollWheelZoom={false}
+						style={{ height: "500px", width: "1000px" }}
+					>
+						<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+						<Marker position={initialPosition}>
+							<Popup>
+								Birendra Multiple Campus <br /> Naubigha Bhawan.
+							</Popup>
+						</Marker>
+					</MapContainer>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Dashboard;
